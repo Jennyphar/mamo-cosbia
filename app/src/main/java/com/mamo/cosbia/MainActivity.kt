e434d490c66c5691910fd65d36035ed09393cd98
Mamqeny <Mamqeny10@gmail.com>
2020-08-14 12:54:44 +0200

    Progressing with Questions

--- app/src/main/java/com/mamo/cosbia/MainActivity.kt
+++ app/src/main/java/com/mamo/cosbia/MainActivity.kt
@@ -13,17 +13,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        window.decorView.systemUiVisibility = SYSTEM_UI_FLAG_FULLSCREEN
+       // window.decorView.systemUiVisibility = SYSTEM_UI_FLAG_FULLSCREEN
         // Validation that a user has entered something
+        val intent = Intent()
         btn_start.setOnClickListener {
             if(et_name.text.toString().isEmpty()){ // checking if the text is empty
                 Toast.makeText(this,
                     "please enter your name",Toast.LENGTH_SHORT).show()
-            }else{ // if the screenshot is not empty then we move to the activity
-                val intent = Intent(this, GeneralThreatsQuestions::class.java)
-                intent.putExtra(Constants.USER_NAME,et_name.text.toString())
-                startActivity(intent) //this starts an intent upon clicking  the start button, which takes us to the General Threats Questions.
-                finish()
+            }else{
+                if(intent == (Intent(this, GeneralThreatsQuestions::class.java))){
+                    intent.putExtra(Constants.USER_NAME,et_name.text.toString())
+                    startActivity(intent) //this starts an intent upon clicking  the start button, which takes us to the General Threats Questions.
+                    finish()
+
+                }else {
+                    // if the text box is not empty then we move to the activity
+                    intent.putExtra(Constants.USER_NAME, et_name.text.toString())
+                    startActivity(intent) //this starts an intent upon clicking  the start button, which takes us to the General Threats Questions.
+                    finish()
+                }
+                
             }
         }
     }
